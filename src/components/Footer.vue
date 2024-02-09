e102913c50e6151f10380466fa15a2b8fdb4e8ae
makmach <makmach1122@gmail.com>
2024-02-09 21:08:26 +0700

    ២. អនុវត្តន៍ការឌីហ្សាញ Footer Last Part

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="grid grid-cols-2 p-5 bg-white shadow h-auto">
-    <div>
+  <div class="grid grid-cols-2 lg:grid-cols-4 p-5 bg-white shadow h-auto">
+    <div class="lg:border-r-2 border-gray-200 lg:mr-5">
       <h3 class="font-semibold text-gray-700 mb-3">Customer Services</h3>
       <ul class="text-gray-500 font-thin">
         <li>Help Center</li>
@@ -8,7 +8,7 @@
         <li>Submite a Dispute</li>
       </ul>
     </div>
-    <div>
+    <div class="lg:border-r-2 border-gray-200 lg:mr-5">
       <h3 class="font-semibold text-gray-700 mb-3">About US</h3>
       <ul class="text-gray-500 font-thin">
         <li>Help Center</li>
@@ -16,21 +16,25 @@
         <li>Submite a Dispute</li>
       </ul>
     </div>
-    <div>
+    <div class="col-span-2 mt-3 lg:mt-0">
       <h3 class="font-semibold text-gray-700 mb-3">Contact US</h3>
-      <ul class="text-gray-500 font-thin">
-        <div class="flex space-x-2">
-            <img class="w-10 h-10 shadow-lg rounded-full hover:shadow-sm cursor-pointer" src="@/assets/images/fb.png" alt="">
-            <img class="w-10 h-10 shadow-lg rounded-full hover:shadow-sm cursor-pointer" src="@/assets/images/twitter.png" alt="">
-            <img class="w-10 h-10 shadow-lg rounded-full hover:shadow-sm cursor-pointer" src="@/assets/images/tiktok.png" alt="">
+        <div class="space-y-2 grid sm:grid-cols-3 sm:space-y-0 sm:gap-4">
+            <div class="flex space-x-2 sm:border-r-2 border-gray-200">
+                <img class="w-10 h-10 shadow-lg rounded-full hover:shadow-sm cursor-pointer" src="@/assets/images/fb.png"/>
+                <img class="w-10 h-10 shadow-lg rounded-full hover:shadow-sm cursor-pointer" src="@/assets/images/twitter.png"/>
+                <img class="w-10 h-10 shadow-lg rounded-full hover:shadow-sm cursor-pointer" src="@/assets/images/tiktok.png"/>
+            </div>
+            <div class="space-y-2">
+                <div class="flex items-center space-x-2">
+                    <img class="w-10 h-10 shadow-lg rounded-full hover:shadow-sm cursor-pointer" src="@/assets/images/phone.png"/>
+                    <span class="text-gray-400">093 973 138</span>
+                </div>
+                <div class="flex items-center space-x-2">
+                    <img class="w-10 h-10 shadow-lg rounded-full hover:shadow-sm cursor-pointer" src="@/assets/images/ms.png"/>
+                    <span class="text-gray-400">makmach1122@gmail.com</span>
+                </div>
+            </div>
         </div>
-        <div>
-            <img class="w-10 h-10 shadow-lg rounded-full hover:shadow-sm cursor-pointer" src="@/assets/images/phone.png" alt="">
-        </div>
-        <div>
-            <img class="w-10 h-10 shadow-lg rounded-full hover:shadow-sm cursor-pointer" src="@/assets/images/ms.png" alt="">
-        </div>
-      </ul>
     </div>
   </div>
 </template>
