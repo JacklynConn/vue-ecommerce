98c934c5ac81416a3a4794a6a2b7099aedde0705
makmach <makmach1122@gmail.com>
2024-02-09 19:44:22 +0700

    ១. អនុវត្តន៍ការឌីហ្សាញ Navbar (Phone Screen without User)

---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="p-5 bg-white flex justify-center items-center shadow">
+    <div class="w-full">
+      <img
+        class="w-30 cursor-pointer object-cover"
+        src="@/assets/images/logo.png"
+        alt=""
+      />
+    </div>
+
+    <div class="flex justify-end space-x-5 text-indigo-700">
+      <div class="cursor-pointer hover:text-indigo-900">Category</div>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 cursor-pointer hover:text-indigo-900"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
+        </svg>
+      </div>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="w-6 h-6 cursor-pointer hover:text-indigo-900"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3 4.25A2.25 2.25 0 0 1 5.25 2h5.5A2.25 2.25 0 0 1 13 4.25v2a.75.75 0 0 1-1.5 0v-2a.75.75 0 0 0-.75-.75h-5.5a.75.75 0 0 0-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 0 0 .75-.75v-2a.75.75 0 0 1 1.5 0v2A2.25 2.25 0 0 1 10.75 18h-5.5A2.25 2.25 0 0 1 3 15.75V4.25Z"
+            clip-rule="evenodd"
+          />
+          <path
+            fill-rule="evenodd"
+            d="M19 10a.75.75 0 0 0-.75-.75H8.704l1.048-.943a.75.75 0 1 0-1.004-1.114l-2.5 2.25a.75.75 0 0 0 0 1.114l2.5 2.25a.75.75 0 1 0 1.004-1.114l-1.048-.943h9.546A.75.75 0 0 0 19 10Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
